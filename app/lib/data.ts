36c7cd6a2fe977b49dcefb54fbda67be1746b11a
AdiletAldazharbekov <aldajarbekov@gmail.com>
2024-01-21 23:23:00 +0600

    add search and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,7 +22,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -39,8 +39,7 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
-
-    await new Promise((resolve) => setTimeout(resolve, 6000));
+    // await new Promise((resolve) => setTimeout(resolve, 6000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
